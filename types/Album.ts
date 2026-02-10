3e20e26f3646b1bb106419835c017557ed67878e
EObed <obedeffum10@gmail.com>
2026-02-10 11:34:30 +0000

    Refactored existing components

--- types/Album.ts
+++ types/Album.ts
@@ -4,6 +4,7 @@ export type Album = {
     title: string
     slug: {current: string}
     coverImage: SanityImageSource
+    photos: SanityImageSource[]
     description?: string
     category?: string
 }
